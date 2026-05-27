eabc7c1f1f204f8fc717d5a39a697cd62cb9c74b
AHMED YASSINE BAHRA <yassinebahra051@gmail.com>
2026-05-27 21:44:10 +0100

    ci(backend): integrate SonarCloud and JaCoCo code coverage

--- platforme_etude_backend/src/test/java/com/sge/platforme_etude/PlatformeEtudeApplicationTests.java
+++ platforme_etude_backend/src/test/java/com/sge/platforme_etude/PlatformeEtudeApplicationTests.java
@@ -1,10 +1,8 @@
 package com.sge.platforme_etude;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PlatformeEtudeApplicationTests {
+class PlatformeEtudeApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
